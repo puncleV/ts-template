eb36975587a868a87f716ad0b30d81e9a4bd97b9
punkkk <punkrus18@gmail.com>
2020-02-12 16:24:25 +0300

    add: cleaning up script for `casual` lib

--- bin/set-up.sh
+++ bin/set-up.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-cd ../
-
 rm -rf ./bin
+
+if [[ $1 == '-c' ]]
+  then
+  npm remove casual
+  sed -i 's/import \*.*//' ./test/greeter.test.ts
+  sed -i 's/casual.name/"world"/' ./test/greeter.test.ts
+  git commit -am 'remove'
+fi
+
 rm -rf ./.git
 
 git init
